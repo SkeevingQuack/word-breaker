8e78d30d501b25d2e53d828281d66e913d8f21b9
Brad Aylsworth <brad.aylsworth@gmail.com>
2018-11-21 14:26:23 -0600

    Player sorta functional

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,10 +1,56 @@
 use ::candidate::{Letter, Candidate};
 use ::wordlist::Wordlist;
 
-pub fn play() {
-    let list = Wordlist::new();
+use std::io;
 
+pub fn play() {
+    let mut list = Wordlist::new();
+    println!("h for help");
     
+    loop {
+        let mut command = String::new();
+        io::stdin().read_line(&mut command)
+            .expect("Failed to read line");
+        let mut command: Vec<&str> = command.as_str().split(" ").collect();
+        match command.remove(0).trim() {
+            "n" => {
+                println!("{}", list.get_word().expect("list empty"));
+            }
+            "i" => {
+                for arg in command {
+                    let arg = arg.trim();
+                    if arg.len() > 1 { panic!("'{}' only 1 char", arg); }
+                    list.include(arg);
+                }
+                println!("{}", list.get_word().expect("list empty"));
+            }
+            "e" => {
+                for arg in command {
+                    let arg = arg.trim();
+                    if arg.len() > 1 { panic!("'{}' only 1 char", arg); }       
+                    list.exclude(arg);
+                }
+                println!("{}", list.get_word().expect("list empty"));
+            }
+            "r" => {
+                list.reset_list();
+                println!("{}", list.get_word().expect("list empty"));
+            }
+            "h" => {
+                println!("n: get New word without changing list");
+                println!("i: limit list to words that Include letters");
+                println!("e: limit list to words that Exclude letters");
+                println!("r: Reset list");
+                println!("q: Quit");
+            }
+            "q" => {
+                return;
+            }
+            _ => {
+                println!("Command not understood: nierq");
+            }
+        }
+    }
 }
 
 #[cfg(test)]
--- src/wordlist/mod.rs
+++ src/wordlist/mod.rs
@@ -1,36 +1,72 @@
 use std::fs::File;
-//use rand::seq::SliceRandom;
+
 use ::rand::Rng;
 use ::rand;
 use std::io;
 use std::io::Read;
 use std::io::Seek;
 
-use ::candidate::Candidate;
+use rand::seq::IteratorRandom;
+use ::candidate::{Candidate, Letter};
 
-struct Wordlist {
+pub struct Wordlist {
     _name: String,
     _source: String,
+    rng: rand::rngs::ThreadRng,
+    full_list: Vec<String>,
+    //TODO: Make list references to full_list
+    // maybe raw pointer?
     list: Vec<String>
 }
 
 impl Wordlist {
-    fn new() -> Wordlist {
+    pub fn new() -> Wordlist {
         let mut contents = String::new();
-        File::open("src/wordlist/legacy.txt")
+        //File::open("src/wordlist/legacy.txt")
+        File::open("src/wordlist/final.txt")  
             .expect("Could not open file")
             .read_to_string(&mut contents)
             .expect("Couldn't put in String");
-        let mut list = Vec::new();
+        let mut full_list = Vec::new();
         for word in contents.split("\n") {
-            list.push(word.to_string());
+            let word = word.to_string();
+            full_list.push(word);
         }
         
         Wordlist {
             _name: "legacy".to_string(),
             _source: "src/wordlist/legacy.txt".to_string(),
-            list: list
+            rng: rand::thread_rng(),
+            list: full_list.clone(),
+            full_list
         }
+    }
+    pub fn get_word(&mut self) -> Option<Candidate> {
+        match self.list.iter().choose(&mut self.rng) {
+            Some(word) => Some(Candidate::from_str(word.as_str())),
+            None => None
+        }
+    }
+    pub fn reset_list(&mut self) {
+        // let newlist = Vec::new();
+        // for word in self.full_list {
+        //     newlist.push(&word[..]);
+        // }
+        // self.list = newlist;
+        self.list = self.full_list.clone()
+    }
+    //pub fn exclude(&mut self, letter: Letter) {
+    //    let letter = String::from(letter.get_alphabet_index() as u8 + 0x61);
+    pub fn exclude(&mut self, letter: &str) {
+        self.list.retain(|e| !e.contains(letter));
+        // for (i, word) in self.list.iter().enumerate() {
+        //     if !word.contains(letter) {
+        //         self.list.remove(i);
+        //     }
+        // }
+    }
+    pub fn include(&mut self, letter: &str) {
+        self.list.retain(|e| e.contains(letter));
     }
 }
         
@@ -58,10 +94,12 @@ mod tests {
     #[allow(unused_imports)]
     use super::*;
 
+    //List runs out of options has weird error
+
     #[test]
     fn new_good1() {
         let list = Wordlist::new();
-        assert_eq!("abets".to_string(), list.list[0]);
+        assert_eq!("abets".to_string(), list.list[0].clone());
     }
     
     #[test]
